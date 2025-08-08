7d1fc16816f27e7f544fdef7e713ac483dd2637e
Jakob Baumfalk <j.baumfalk@yahoo.de>
2025-08-08 16:49:22 +0200

    finished ex04

--- CPP01/ex04/manipulator.cpp
+++ CPP01/ex04/manipulator.cpp
@@ -6,23 +6,44 @@
 /*   By: jbaumfal <jbaumfal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 23:01:51 by jbaumfal          #+#    #+#             */
-/*   Updated: 2025/08/05 19:46:09 by jbaumfal         ###   ########.fr       */
+/*   Updated: 2025/08/08 16:41:42 by jbaumfal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "manipulator.hpp"
+
+
 
 void	manipulator(std::ifstream &file, std::string filename,  std::string s1, std::string s2)
 {
 	std::string		line;
 	std::ofstream	replace_file;
 	std::string		replace_file_name;
+	size_t			cursor;
+	size_t			finder;
+	size_t			s1_len;
+	size_t			s2_len;
 
 	replace_file_name = filename + ".replace";
 	replace_file.open(replace_file_name.c_str());
-	
-	while (getline(file, line))
+	s1_len = s1.length();
+	s2_len = s2.length();
+
+	while (std::getline(file, line))
 	{
-		
+		cursor = 0;
+		while(1)
+		{
+			finder = line.find(s1, cursor);
+			if (finder != std::string::npos)
+			{
+				line.erase(finder, s1_len);
+				line.insert(finder, s2);
+				cursor = finder + s2_len;
+			}
+			else
+				break ;
+		}
+		replace_file << line << std::endl;
 	}
 }
