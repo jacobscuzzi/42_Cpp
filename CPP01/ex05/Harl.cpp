a2062f81eb0a01dd232cdf5589fb4df9dd87f834
Jakob Baumfalk <j.baumfalk@yahoo.de>
2025-08-26 13:45:42 +0200

    finished exercise ex05

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -8,25 +8,29 @@ Harl::Harl( void )
 		
 Harl::~Harl( void )
 {
-
+	std::cout << "Its to late, Harl dedtroyed itself" << std::endl;
 }
 
 
 void Harl::debug( void )
 {
 	std::cout << "I dont know why im so grumpy" << std::endl;
+	sleep(2);
 }
 void Harl::info( void )
 {
 	std::cout << "Something to eat would be nice no?"<< std::endl;
+	sleep(2);
 }
 void Harl::warning( void )
 {
 	std::cout << "I really need food , right now, im hungryyyy" << std::endl;
+	sleep(2);
 }
 void Harl::error( void )
 {
 	std::cout << "Im starving its to late" << std::endl;
+	sleep(2);
 }
 
 void Harl::complain( std::string level)
@@ -41,17 +45,17 @@ void Harl::complain( std::string level)
 
 	void (Harl::*functions[]) (void) =
 	{
-		&Harl::debug(),
-		&Harl::info(),
-		&Harl::warning(),
-		&Harl::error()
-	}
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
 
-	for(int i;  = 0; i++ < 4)
+	for (int i = 0; i < 4; i++)
 	{
 		if (level == levels[i])
 		{
-			this->*functions[i];
+			(this->*functions[i])();
 			return ;
 		}
 	}
--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -3,6 +3,7 @@
 
 # include <iostream>
 # include <string>
+#include <unistd.h>
 
 class Harl
 {
